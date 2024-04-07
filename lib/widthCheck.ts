d4ee8ed2a452fc9c69afd99a573642c8c4cf2416
vaas2k <sk315425@gmail.com>
2024-04-07 15:04:09 +0500

    build fixes

--- lib/widthCheck.ts
+++ lib/widthCheck.ts
@@ -3,17 +3,16 @@ import { useState,useEffect } from "react";
 
 export const useWidth = () => {
 
-  if(typeof window !== 'undefined'){
     const [width, setWidth] = useState(window.innerWidth);
     useEffect(() => {
       function chngWidth() {
         setWidth(window.innerWidth);
       }
       window.addEventListener("resize", chngWidth);
-      return () => window.removeEventListener("resize", chngWidth);
+      return () => {window.removeEventListener("resize", chngWidth)
+
+      return width;
+      };
     }, []);
-    
-    return width;
-  }
     
 }
