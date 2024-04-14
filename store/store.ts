ced25f770324c271e3dabda026ca1d43eb145b17
vaas2k <sk315425@gmail.com>
2024-04-14 16:17:07 +0500

    nothing much

--- store/store.ts
+++ store/store.ts
@@ -29,5 +29,6 @@ const store = configureStore({
 });
 
 const persistor = persistStore(store)
-
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 export { store , persistor };
